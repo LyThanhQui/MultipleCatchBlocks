d1b55d9f5c50e675f63469c47395fe268153f9b8
Qui <lquiitfuture@gmail.com>
2019-12-25 17:13:59 +0700

    Add project

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MultipleCatchBlocks
+{
+    class Program
+    {
+        //Example1: Program to show how to implement multiple catch blocks in C#.
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("ENTER ANY TWO NUBERS");
+            try
+            {
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+                c = a / b;
+                Console.WriteLine("C VALUE = " + c);
+            }
+           // catch (Exception ex)
+            //{
+          //      Console.WriteLine("Catch Exeption");
+       //    }
+            catch (DivideByZeroException dbze)
+            {
+                Console.WriteLine("second number should not be zero");
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("enter only integer numbers");
+            }
+            Console.ReadKey();
+        }
+
+        
+    }
+}
